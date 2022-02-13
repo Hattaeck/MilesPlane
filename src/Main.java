9b6ece65fbe86ecb71ac2df73c4ec3610d625ebb
ALEKSEY <hattaeck@gmail.com>
2022-02-13 13:38:06 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        int priceTicket = 10000;
+        int miles = priceTicket / 20;
+        System.out.println(miles);
+
+    }
+}
